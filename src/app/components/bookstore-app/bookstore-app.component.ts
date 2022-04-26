7f51868f42b09600350b2f001df3f2eca5545f11
DaniloPorto30 <dprosa23@gmail.com>
2022-04-26 20:42:16 -0300

    Criado metodo adicionar livro

--- src/app/components/bookstore-app/bookstore-app.component.ts
+++ src/app/components/bookstore-app/bookstore-app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-bookstore-app',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookstoreAppComponent implements OnInit {
 
+  livroDetail !: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clickAddlivros() {
+    this.livroDetail.reset();
+  }
 }
